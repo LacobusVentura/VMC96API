300ad236425c702cac15787adb4e3df08fd2d563
Tiago Ventura <tiago.ventura@gmail.com>
2018-12-17 14:45:53 -0200

    Code Indentation

--- vmc96.h
+++ vmc96.h
@@ -1,34 +1,32 @@
 /*
      MoneyFlex.net
-      
+
      VMC96 Module - http://www.moneyflex.net/vmc96/
-     
-     Device Control Library 
- 
+
+     Device Control Library
+
      Author: Tiago Ventura
      Contact: tiago.ventura@gmail.com
      Date: Dec/2019
- 
+
 */
 
 #ifndef __VMC96_H__
 #define __VMC96_H__
 
 
-#define VMC96_SUCCESS                         (0)
-#define VMC96_ERROR_OUT_OF_MEMORY             (1)
-
-#define VMC96_ERROR_FTDI_INITIALIZE           (101)
-#define VMC96_ERROR_FTDI_SET_INTERFACE        (102)
-#define VMC96_ERROR_FTDI_OPEN_USB_DEVICE      (103)
-#define VMC96_ERROR_FTDI_RESET_USB            (104)
-#define VMC96_ERROR_FTDI_SET_BAUDRATE         (105)
-#define VMC96_ERROR_FTDI_SET_LINE_PROPS       (106)
-#define VMC96_ERROR_FTDI_SET_NO_FLOW          (107)
-#define VMC96_ERROR_FTDI_WRITE_DATA           (108)
-#define VMC96_ERROR_FTDI_READ_DATA            (109)
-#define VMC96_ERROR_FTDI_PURGE_BUFFERS        (110)
-
+#define VMC96_SUCCESS                          (0)
+#define VMC96_ERROR_OUT_OF_MEMORY              (1)
+#define VMC96_ERROR_FTDI_INITIALIZE            (101)
+#define VMC96_ERROR_FTDI_SET_INTERFACE         (102)
+#define VMC96_ERROR_FTDI_OPEN_USB_DEVICE       (103)
+#define VMC96_ERROR_FTDI_RESET_USB             (104)
+#define VMC96_ERROR_FTDI_SET_BAUDRATE          (105)
+#define VMC96_ERROR_FTDI_SET_LINE_PROPS        (106)
+#define VMC96_ERROR_FTDI_SET_NO_FLOW           (107)
+#define VMC96_ERROR_FTDI_WRITE_DATA            (108)
+#define VMC96_ERROR_FTDI_READ_DATA             (109)
+#define VMC96_ERROR_FTDI_PURGE_BUFFERS         (110)
 #define VMC96_ERROR_RESPONSE_INVALID_CHECKSUM  (201)
 #define VMC96_ERROR_RESPONSE_NEGATIVE_ACK      (202)
 #define VMC96_ERROR_RESPONSE_MALFORMED         (203)
@@ -61,7 +59,7 @@ extern "C"
 	int vmc96_motor_get_version( VMC96_t * vmc96, char * version );
 	int vmc96_motor_driver_setup( VMC96_t * vmc96, unsigned char mode );
 	int vmc96_motor_reset( VMC96_t * vmc96 );
-	
+
 	int vmc96_motor_status_request( VMC96_t * vmc96 );
 	int vmc96_motor_scan_array( VMC96_t * vmc96 );
 	int vmc96_motor_stop_all( VMC96_t * vmc96 );
@@ -72,7 +70,7 @@ extern "C"
 #ifdef __cplusplus
 }
 #endif
- 
+
 #endif
 
 /* eof */
