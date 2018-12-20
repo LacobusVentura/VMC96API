c271d830fd6f49b5e99f03ce5c8bec87a29bb672
Tiago Ventura <tiago.ventura@gmail.com>
2018-12-20 11:19:09 -0200

    Update vmc96api.h

--- vmc96api.h
+++ vmc96api.h
@@ -4,7 +4,7 @@
 	\author Tiago Ventura (tiago.ventura@gmail.com)
 	\date Dec/2019
 
-    VMC96 Module - http://www.moneyflex.net/vmc96/
+	VMC96 Module - http://www.moneyflex.net/vmc96/
 */
 
 #ifndef __VMC96_H__
@@ -13,28 +13,31 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define VMC96_SUCCESS                          (0)
-#define VMC96_ERROR_OUT_OF_MEMORY              (1)
-#define VMC96_ERROR_FTDI_INITIALIZE            (101)
-#define VMC96_ERROR_FTDI_SET_INTERFACE         (102)
-#define VMC96_ERROR_FTDI_OPEN_USB_DEVICE       (103)
-#define VMC96_ERROR_FTDI_RESET_USB             (104)
-#define VMC96_ERROR_FTDI_SET_BAUDRATE          (105)
-#define VMC96_ERROR_FTDI_SET_LINE_PROPS        (106)
-#define VMC96_ERROR_FTDI_SET_NO_FLOW           (107)
-#define VMC96_ERROR_FTDI_WRITE_DATA            (108)
-#define VMC96_ERROR_FTDI_READ_DATA             (109)
-#define VMC96_ERROR_FTDI_PURGE_BUFFERS         (110)
-#define VMC96_ERROR_RESPONSE_INVALID_CHECKSUM  (201)
-#define VMC96_ERROR_RESPONSE_NEGATIVE_ACK      (202)
-#define VMC96_ERROR_RESPONSE_MALFORMED         (203)
-#define VMC96_ERROR_RESPONSE_INVALID_SOURCE    (204)
-#define VMC96_ERROR_RESPONSE_INVALID_LENGTH    (205)
-#define VMC96_ERROR_INVALID_MOTOR_COORDINATES  (301)
+#define VMC96_SUCCESS                              (0)
+#define VMC96_ERROR_OUT_OF_MEMORY                  (1)
+#define VMC96_ERROR_FTDI_INITIALIZE                (101)
+#define VMC96_ERROR_FTDI_SET_INTERFACE             (102)
+#define VMC96_ERROR_FTDI_OPEN_USB_DEVICE           (103)
+#define VMC96_ERROR_FTDI_RESET_USB                 (104)
+#define VMC96_ERROR_FTDI_SET_BAUDRATE              (105)
+#define VMC96_ERROR_FTDI_SET_LINE_PROPS            (106)
+#define VMC96_ERROR_FTDI_SET_NO_FLOW               (107)
+#define VMC96_ERROR_FTDI_WRITE_DATA                (108)
+#define VMC96_ERROR_FTDI_READ_DATA                 (109)
+#define VMC96_ERROR_FTDI_PURGE_BUFFERS             (110)
+#define VMC96_ERROR_RESPONSE_INVALID_CHECKSUM      (201)
+#define VMC96_ERROR_RESPONSE_NEGATIVE_ACK          (202)
+#define VMC96_ERROR_RESPONSE_MALFORMED             (203)
+#define VMC96_ERROR_RESPONSE_INVALID_SOURCE        (204)
+#define VMC96_ERROR_RESPONSE_INVALID_LENGTH        (205)
+#define VMC96_ERROR_INVALID_MOTOR_COORDINATES      (301)
 
-#define VMC96_VERSION_STRING_MAX_LEN           (32)
-#define VMC96_MOTOR_ARRAY_ROWS_COUNT           (8)
-#define VMC96_MOTOR_ARRAY_COLUMNS_COUNT        (12)
+#define VMC96_OPTO_LINE_SAMPLE_BLOCK_LENGTH_US     (1280000L) /* 1.28s block*/
+#define VMC96_OPTO_LINE_SAMPLE_LENGTH_US           (40000L)   /* 40ms sample */
+#define VMC96_OPTO_LINE_SAMPLES_PER_BLOCK          (32)       /* 32 samples  per block */
+#define VMC96_VERSION_STRING_MAX_LEN               (32)
+#define VMC96_MOTOR_ARRAY_ROWS_COUNT               (8)
+#define VMC96_MOTOR_ARRAY_COLUMNS_COUNT            (12)
 
 
 typedef struct VMC96_s VMC96_t;
@@ -83,6 +86,7 @@ extern "C"
 	int vmc96_motor_pair_run( VMC96_t * vmc96, unsigned char row, unsigned char col1, unsigned char col2 );
 
 	int vmc96_motor_opto_line_status( VMC96_t * vmc96, uint32_t * status );
+
 
 #ifdef __cplusplus
 }
